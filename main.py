19bc37fb44d4b7719c8d734d0e8bb23fec0b6fca
21sarage <106278056+21sarage@users.noreply.github.com>
2025-01-07 11:36:21 +0100

    testing git

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from ortools.sat.python import cp_model
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 def solve_scheduling_problem(df):
