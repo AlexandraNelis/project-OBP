bfc0d78ad281f2ecd178d15cde6dbfd04f6dc6af
Lara Owobowale <laraowobowale@gmail.com>
2025-01-28 02:07:59 +0100

    added the extended solver for gurobi

--- main.py
+++ main.py
@@ -298,17 +298,17 @@ def generate_test_case(num_jobs, num_machines):
 
     return pd.DataFrame(data)
 
-def evaluate_solver(max_jobs,max_machines,time_limit,batch):
+def evaluate_solver(min_jobs,max_jobs,min_machines,max_machines,time_limit,batch,Solver):
     """
     Run the solver with increasing job and machine sizes, and record results.
     """
     results = []
     largest_set_of_jobs = {}
       
-    for num_machines in range(2, max_machines + 1): # Increment machines by 1
+    for num_machines in range(min_machines, max_machines + 1): # Increment machines by 1
         best_job = 0
         largest_set_of_jobs[num_machines]=best_job        
-        for num_jobs in range(2, max_jobs + 1, 2):# Increment jobs by 10 
+        for num_jobs in range(min_jobs, max_jobs + 1, 2):# Increment jobs by 10 
             st.write(f"Testing with {num_jobs} jobs and {num_machines} machines...")
             df = generate_test_case(num_jobs, num_machines)
             solving_set=[]
@@ -318,7 +318,10 @@ def evaluate_solver(max_jobs,max_machines,time_limit,batch):
                 st.write(f"trial {i+1}")
                 machine_columns = [f"Machine {i}" for i in range(1, num_machines + 1)]
                 start_time = time.time()
-                result = solve_scheduling_problem(df, machine_columns)
+                if Solver:
+                    result = solve_scheduling_problem(df, machine_columns)
+                else:
+                    result = solve_gurobi_scheduling_problem(df, machine_columns)
                 end_time = time.time()
                 solving_time =  end_time - start_time
                 if solving_time >= time_limit:
@@ -395,7 +398,7 @@ def compare_solvers(test_jobs,test_machines):
                 st.write(f"trial {i+1}")
                 machine_columns = [f"Machine {i}" for i in range(1, num_machines + 1)]
                 or_solving_set = run_batch(df, machine_columns,True,or_solving_set,time_limit)
-                gur_solving_set = run_batch(df, machine_columns,True,gur_solving_set,time_limit)
+                gur_solving_set = run_batch(df, machine_columns,False,gur_solving_set,time_limit)
 
             or_false_count = sum(1 for _, is_false, _ in or_solving_set if not is_false)
             gur_false_count =  sum(1 for _, is_false, _ in gur_solving_set if not is_false)
@@ -450,13 +453,15 @@ def create_download_link(val, filename,type):
 
 option = st.selectbox(
     "Choose an option:",  # Label for the dropdown
-    ["Extended solver OR Tools", "Compare Gurobi and OR tools"]  # List of options
+    ["Extended solver OR Tools", "Extended solver Gurobi"]  # List of options
 )
 if option == "Extended solver OR Tools":
     st.write("You chose Extended solver OR Tools! 🎉")
      # User input fields (before button)
-    num_jobs = st.number_input("Maximum number of jobs", min_value=1, step=1)
-    num_machines = st.number_input("Maximum number of machines", min_value=1, step=1)
+    min_num_jobs = st.number_input("Minimum number of jobs", min_value=1, step=1)
+    max_num_jobs = st.number_input("Maximum number of jobs", min_value=1, step=1)
+    min_machines = st.number_input("Minimum number of machines", min_value=1, step=1)
+    max_machines = st.number_input("Maximum number of machines", min_value=1, step=1)
     time_limit = st.number_input("Search time limit (seconds)", min_value=1, step=1)
     batch = st.number_input("Batch size of trial runs per instance", min_value=1, step=1)
 
@@ -464,7 +469,7 @@ if option == "Extended solver OR Tools":
     if st.button("Run Solver Performance Tests"):
         with st.spinner("Running tests..."):
             performance_results, largest_set = evaluate_solver(
-                int(num_jobs), int(num_machines), int(time_limit), int(batch)
+                int(min_num_jobs),int(max_num_jobs), int(min_machines),int(max_machines), int(time_limit), int(batch),True
             )
             st.markdown("### Performance Results")
             st.dataframe(performance_results)
@@ -488,65 +493,40 @@ if option == "Extended solver OR Tools":
             # Optionally save results to a CSV
             csv_data = performance_results.to_csv(index=False).encode("utf-8")
             largest_set_data = largest_set.to_csv(index=False).encode("utf-8")
-            performance_url = create_download_link(csv_data, 'Performance results',"csv")
-            largest_set_url =create_download_link(largest_set_data,"Largest jobset","csv")
-            graph_url = create_download_link(png_data,"Bar chart","png")
+            performance_url = create_download_link(csv_data, 'OR tools Performance results',"csv")
+            largest_set_url =create_download_link(largest_set_data,"OR tools Largest jobset","csv")
+            graph_url = create_download_link(png_data,"Bar chart OR tools","png")
             st.markdown(performance_url, unsafe_allow_html=True)
             st.markdown(largest_set_url, unsafe_allow_html=True)
             st.markdown(graph_url, unsafe_allow_html=True)
 
-elif option == "Compare Gurobi and OR tools":
-    st.write("You chose Compare Gurobi and OR tools! 🚀")
-    if st.button("Run Comparison solver Tests"):
+elif option == "Extended solver Gurobi":
+    st.write("You chose Extended solver Gurobi! 🚀")
+    # User input fields (before button)
+    min_num_jobs = st.number_input("Minimum number of jobs", min_value=1, step=1)
+    max_num_jobs = st.number_input("Maximum number of jobs", min_value=1, step=1)
+    min_machines = st.number_input("Minimum number of machines", min_value=1, step=1)
+    max_machines = st.number_input("Maximum number of machines", min_value=1, step=1)
+    time_limit = st.number_input("Search time limit (seconds)", min_value=1, step=1)
+    batch = st.number_input("Batch size of trial runs per instance", min_value=1, step=1)
+
+    # Run solver only when button is clicked
+    if st.button("Run Solver Performance Tests"):
         with st.spinner("Running tests..."):
-            test_jobs = [2,3]
-            test_machines = [2,5]
-            or_performance_results,gur_performance_results,or_largest_set,gur_largest_set = compare_solvers(test_jobs,test_machines)
-            st.markdown("### Performance Results")
-            st.dataframe(or_performance_results)
-            st.dataframe(gur_performance_results)
-            print(or_largest_set)
-            print(gur_largest_set)
-            st.markdown("### Largest number of jobs per machine")
-            
-
-            combined_data = pd.merge(or_largest_set, gur_largest_set, on="Number of machines")
-
-            # Reshape the DataFrame to a long format
-            combined_data_melted = combined_data.melt(id_vars=["Number of machines"], var_name="Solver", value_name="Number of jobs")
-
-            # Display the DataFrame
-            st.write(combined_data_melted)  # Optional: View the transformed DataFrame
-
-            # Create a grouped bar chart using Altair
-            chart = alt.Chart(combined_data_melted).mark_bar().encode(
-                x=alt.X("Number of machines:O", title="Number of Machines"),  # Convert to categorical
-                y=alt.Y("Number of jobs:Q", title="Number of Jobs"),
-                color="Solver:N",  # Color by solver
-                xOffset="Solver:N"  # Offset bars to make them appear side by side
-            ).properties(
-                width=100  # Adjust width for clarity
+            performance_results, largest_set = evaluate_solver(
+                int(min_num_jobs),int(max_num_jobs), int(min_machines),int(max_machines), int(time_limit), int(batch),False
             )
-
-            # Display the Altair chart in Streamlit
-            st.altair_chart(chart, use_container_width=True)
+            st.markdown("### Performance Results")
+            st.dataframe(performance_results)
+            st.markdown("### Largest number of jobs per machine")
+            st.dataframe(largest_set)
+            st.bar_chart(largest_set, x = 'Number of machines', y = 'Number of jobs', x_label= "number of machines", y_label= "number of jobs")
+            graph_data =largest_set.set_index('Number of machines')
             fig, ax = plt.subplots()
-            # Plot with "hue" by setting `x`, `y`, and `hue`
-            sns.barplot(
-                data=combined_data_melted, 
-                x="Number of machines", 
-                y="Number of jobs", 
-                hue="Solver",  # Differentiate bars by "variable" column
-                ax=ax
-                )   
-
-            ax.set_title("Grouped Bar Chart")
+            graph_data.plot(kind="bar", ax=ax, legend=False)
+            ax.set_title("Maximum number of jobs solvable for number of machines")
             ax.set_xlabel("Number of machines")
             ax.set_ylabel("Number of jobs")
-            ax.set_label("Comparison between Gurobi and OR tools per machine")
-            ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
-
-            plt.tight_layout()  # Ensures layout does not cut off elements
 
             # Save the chart to a buffer for downloading
             buffer = io.BytesIO()
@@ -556,19 +536,11 @@ elif option == "Compare Gurobi and OR tools":
 
             
             # Optionally save results to a CSV
-            or_csv_data = or_performance_results.to_csv(index=False).encode("utf-8")
-            gur_csv_data = gur_performance_results.to_csv(index=False).encode("utf-8")
-
-            largest_set_data = combined_data.to_csv(index=False).encode("utf-8")
-            or_performance_url = create_download_link(or_csv_data, 'Performance results OR tools',"csv")
-            gur_performance_url = create_download_link(gur_csv_data, 'Performance results Gurobi',"csv")
-
-            largest_set_url =create_download_link(largest_set_data,"Largest jobset","csv")
-            graph_url = create_download_link(png_data,"Bar chart","png")
-            st.markdown(or_performance_url, unsafe_allow_html=True)
-            st.markdown(gur_performance_url, unsafe_allow_html=True)
+            csv_data = performance_results.to_csv(index=False).encode("utf-8")
+            largest_set_data = largest_set.to_csv(index=False).encode("utf-8")
+            performance_url = create_download_link(csv_data, 'Gurobi Performance results',"csv")
+            largest_set_url =create_download_link(largest_set_data,"Gurobi Largest jobset","csv")
+            graph_url = create_download_link(png_data,"Bar chart Gurobi","png")
+            st.markdown(performance_url, unsafe_allow_html=True)
             st.markdown(largest_set_url, unsafe_allow_html=True)
             st.markdown(graph_url, unsafe_allow_html=True)
-
-
-# Run and display the evaluation results
